f8bcb7094d7986724060d0ab8b0b3e3d834c289c
TuanKiet1106 <100402929+TuanKiet1106@users.noreply.github.com>
2022-04-21 23:09:07 +0700

    Include DirectX libraries. Create Device, SwapChain, Render, Sprite objects. InitDirectx.

--- SuperMarioBros3/main.cpp
+++ SuperMarioBros3/main.cpp
@@ -1,5 +1,8 @@
 #include <windows.h>
 
+#include <d3d10.h>
+#include <d3dx10.h>
+
 #include <stdio.h> //VA_PRINT
 
 
@@ -10,21 +13,18 @@
 #define WINDOW_WIDTH 640
 #define WINDOW_HEIGHT 480
 
+ID3D10Device* pD3DDevice = NULL;
+IDXGISwapChain* pSwapChain = NULL; 
+ID3D10RenderTargetView* pRenderTargetView = NULL;
+ID3DX10Sprite* spriteObject = NULL;
+
+int BackBufferWidth = 0;
+int BackBufferHeight = 0;
+
 HWND hWnd = 0;
 
-LRESULT CALLBACK WinProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
-{
-	switch (message) {
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		break;
-	default:
-		return DefWindowProc(hWnd, message, wParam, lParam);
-	}
-
-	return 0;
-}
-
+//DEBUGGING
+//##################################################
 #define _W(x)  __W(x)
 #define __W(x)  L##x
 
@@ -47,6 +47,131 @@ void DebugOutTitle(const wchar_t* fmt, ...)
 	wchar_t s[1024];
 	VA_PRINTS(s);
 	SetWindowText(hWnd, s);
+}
+//##################################################
+
+void InitDirectX(HWND hWnd)
+{
+	// retrieve client area width & height so that we can create backbuffer height & width accordingly 
+	RECT r;
+	GetClientRect(hWnd, &r);
+
+	BackBufferWidth = r.right + 1;
+	BackBufferHeight = r.bottom + 1;
+
+
+	// Create & clear the DXGI_SWAP_CHAIN_DESC structure
+	DXGI_SWAP_CHAIN_DESC swapChainDesc;
+	ZeroMemory(&swapChainDesc, sizeof(swapChainDesc));
+
+	// Fill in the needed values
+	swapChainDesc.BufferDesc.Width = BackBufferWidth;
+	swapChainDesc.BufferDesc.Height = BackBufferHeight;
+	swapChainDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	swapChainDesc.BufferDesc.RefreshRate.Numerator = 60;
+	swapChainDesc.BufferDesc.RefreshRate.Denominator = 1;
+
+	swapChainDesc.SampleDesc.Count = 1;
+	swapChainDesc.SampleDesc.Quality = 0;
+
+	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	swapChainDesc.BufferCount = 1;
+	swapChainDesc.OutputWindow = hWnd;
+	swapChainDesc.Windowed = TRUE;
+
+	// Create the D3D device and the swap chain
+	HRESULT hr = D3D10CreateDeviceAndSwapChain(NULL, //video adapter - optional
+		D3D10_DRIVER_TYPE_REFERENCE, //Driver type
+		NULL,	//Software
+		0,	//Flags
+		D3D10_SDK_VERSION, //*pFeatureLevels
+		&swapChainDesc, 
+		&pSwapChain,
+		&pD3DDevice);
+
+	if (hr != S_OK)
+	{
+		DebugOut((wchar_t*)L"[ERROR] D3D10CreateDeviceAndSwapChain has failed %s %d", _W(__FILE__), __LINE__);
+		return;
+	}
+
+	// Get the back buffer from the swapchain
+	ID3D10Texture2D* pBackBuffer;
+	hr = pSwapChain->GetBuffer(0, __uuidof(ID3D10Texture2D), (LPVOID*)&pBackBuffer);
+	if (hr != S_OK)
+	{
+		DebugOut((wchar_t*)L"[ERROR] pSwapChain->GetBuffer has failed %s %d", _W(__FILE__), __LINE__);
+		return;
+	}
+
+	// create the render target view
+	hr = pD3DDevice->CreateRenderTargetView(pBackBuffer, NULL, &pRenderTargetView);
+
+	// release the back buffer
+	pBackBuffer->Release();
+
+	// Make sure the render target view was created successfully
+	if (hr != S_OK)
+	{
+		DebugOut((wchar_t*)L"[ERROR] CreateRenderTargetView has failed %s %d", _W(__FILE__), __LINE__);
+		return;
+	}
+
+	// set the render target
+	pD3DDevice->OMSetRenderTargets(1, &pRenderTargetView, NULL);
+
+	// create and set the viewport
+	D3D10_VIEWPORT viewPort;
+	viewPort.Width = BackBufferWidth;
+	viewPort.Height = BackBufferHeight;
+	viewPort.MinDepth = 0.0f;
+	viewPort.MaxDepth = 1.0f;
+	viewPort.TopLeftX = 0;
+	viewPort.TopLeftY = 0;
+	pD3DDevice->RSSetViewports(1, &viewPort);
+
+
+	// create the sprite object to handle sprite drawing 
+	hr = D3DX10CreateSprite(pD3DDevice, 0, &spriteObject);
+
+	// Make sure the sprite creation was successful
+	if (hr != S_OK)
+	{
+		DebugOut((wchar_t*)L"[ERROR] D3DX10CreateSprite has failed %s %d", _W(__FILE__), __LINE__);
+		return;
+	}
+
+	D3DXMATRIX matProjection;
+
+	// Create the projection matrix using the values in the viewport
+	D3DXMatrixOrthoOffCenterLH(&matProjection,
+		(float)viewPort.TopLeftX,
+		(float)viewPort.Width,
+		(float)viewPort.TopLeftY,
+		(float)viewPort.Height,
+		0.1f,
+		10);
+	hr = spriteObject->SetProjectionTransform(&matProjection);
+
+
+
+
+	DebugOut((wchar_t*)L"[INFO] InitDirectX has been successful\n");
+
+	return;
+}
+
+LRESULT CALLBACK WinProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	switch (message) {
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+
+	return 0;
 }
 
 HWND CreateGameWindow(HINSTANCE hInstance, int nCmdShow, int ScreenWidth, int ScreenHeight) {
@@ -98,6 +223,8 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 	hWnd = CreateGameWindow(hInstance, nCmdShow, WINDOW_WIDTH, WINDOW_HEIGHT);
 
 	if (hWnd == 0) return 0;
+
+	InitDirectX(hWnd);
 
 	return 0;
 }
