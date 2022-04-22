feeb2f4503fc5585c45a6a9c6eaa898cfb8ff540
TuanKiet1106 <100402929+TuanKiet1106@users.noreply.github.com>
2022-04-22 07:55:34 +0700

    Remove Debug Functions in main.cpp.

--- SuperMarioBros3/main.cpp
+++ SuperMarioBros3/main.cpp
@@ -52,32 +52,6 @@ float brick_y = BRICK_START_Y;
 
 HWND hWnd = 0; // Window Handler
 
-//DEBUGGING
-//##################################################
-#define _W(x)  __W(x)
-#define __W(x)  L##x
-
-#define VA_PRINTS(s) {				\
-		va_list argp;				\
-		va_start(argp, fmt);		\
-		vswprintf_s(s, fmt, argp);	\
-		va_end(argp);				\
-}		
-
-void DebugOut(const wchar_t* fmt, ...)
-{
-	wchar_t s[4096];
-	VA_PRINTS(s);
-	OutputDebugString(s);
-}
-
-void DebugOutTitle(const wchar_t* fmt, ...)
-{
-	wchar_t s[1024];
-	VA_PRINTS(s);
-	SetWindowText(hWnd, s);
-}
-//##################################################
 
 void InitDirectX(HWND hWnd)
 {
