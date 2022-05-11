c74c8129f8b58a002e97bad5335cbdacabdfa63c
TuanKiet1106 <100402929+TuanKiet1106@users.noreply.github.com>
2022-05-11 21:27:29 +0700

    Fix tile loading.

--- SuperMarioBros3/Tiles.cpp
+++ SuperMarioBros3/Tiles.cpp
@@ -117,7 +117,7 @@ void Tiles::LoadTileset(LPCWSTR config, LPCWSTR tileset) {
 	for (int i = 0; i < row; i++) {
 		for (int j = 0; j < col; j++) {
 			DebugOut(L"[INFO] %i %i %i %i\n", j * this->tileWidth, i * this->tileHeight, this->tileHeight, this->tileWidth);
-			AddTile(j * this->tileWidth, i * this->tileHeight, this->tileHeight, this->tileWidth);
+			AddTile(j * this->tileWidth, i * this->tileHeight, this->tileHeight-1, this->tileWidth-1);
 		}
 	}
 }
